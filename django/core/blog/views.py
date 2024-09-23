5194806ccf38506b3fa2abaa5edbd4957e5ba79f
Shishir <mac@shihsir.local>
2024-09-23 08:28:08 +0545

    just basic one

--- django/core/blog/views.py
+++ django/core/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Blog
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    blog = Blog.objects.all()
+    return render(request, "index.html", {blog: "blog"})
 def home(request):
     return render(request, "blog.html")
 
